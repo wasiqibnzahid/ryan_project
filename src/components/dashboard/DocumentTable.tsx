67a36e422375f5ed96e68309fb2cdc44d7a19241
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2024-10-03 03:52:46 +0500

    Added styling for document category

--- src/components/dashboard/DocumentTable.tsx
+++ src/components/dashboard/DocumentTable.tsx
@@ -7,15 +7,27 @@ import { formatDate } from "@/utils";
 import { Button } from "../ui/button";
 const columnHelper = createColumnHelper<Report>();
 const columns = [
-  columnHelper.accessor("name", {cell(props) {
-      return <div className="font-semibold border-solid border-b-[#919191] text-nowrap border-b-[1px]">{props.getValue()}</div>
-  },}),
+  columnHelper.accessor("name", {
+    cell(props) {
+      return (
+        <div className="font-semibold border-solid border-b-[#919191] text-nowrap border-b-[1px]">
+          {props.getValue()}
+        </div>
+      );
+    },
+  }),
   columnHelper.accessor("description", {
     cell(props) {
       return <div className="text-muted">{props.getValue()}</div>;
     },
   }),
-  columnHelper.accessor("category", {}),
+  columnHelper.accessor("category", {
+    cell: ({ getValue }) => (
+      <Button className="bg-accent-light rounded-[30px] hover:bg-accent-light cursor-default bg-opacity-10 text-accent-foreground">
+        {getValue()}
+      </Button>
+    ),
+  }),
   columnHelper.accessor("modified", {
     cell: ({ getValue }) => <div> {formatDate(getValue())}</div>, // Will be used for formatting later on
   }),
@@ -24,12 +36,15 @@ const columns = [
 export const DocumentTable = () => {
   const { documents } = useDocuments();
   return (
-    <div className="mb-6">
+    <div
+      className="mb-6
+    "
+    >
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold text-accent-foreground">Documents</h2>
         <div className="flex justify-end items-center gap-2">
           <span>All Documents</span>
-          <Button className="bg-[#1694cc] px-1.5 py-0 h-6" >&rarr;</Button>
+          <Button className="bg-[#1694cc] px-1.5 py-0 h-6">&rarr;</Button>
         </div>
       </div>
       <hr className="bg-gradient-to-r from-cyan-400 to-primary opacity-20 h-1 my-5" />
